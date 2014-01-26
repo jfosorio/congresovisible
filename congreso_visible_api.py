93080e063ae27a1645b43a6354f8b78976d7a463
Juan Felipe Osorio <jfosorio@gmail.com>
2014-01-26 22:55:14 +0100

    primera version con las estadisticas por partido

--- congreso_visible_api.py
+++ congreso_visible_api.py
@@ -17,7 +17,9 @@ import datetime
 
 def download_data():
     ''' This is a module to convert all the information in congresovisible
-        to a pickle file'''
+        to a pickle file: 
+            cva.download_data() 
+        '''
     # Initial URL
     url = 'http://congresovisible.org/api/apis/candidatos/'
     req= urllib2.Request(url)
@@ -41,11 +43,45 @@ def download_data():
     today = datetime.date.today()
     filename = str(today)+"_candidates.p"
     pickle.dump(candidates, open(filename, "wb" ) )
-    print('The pickle file'+filename+'has been created!)
+    print('The pickle file: '+filename+' has been created!')
     
    
 def load_data(file='2014-01-25_candidates.p'):
-    candidates = pickle.load( open(file, 'rb' ) )
-    return(candidates)
+    ''' Obtain the data an ad Id number to treat the columns separately when needed'''
+    registers = pickle.load( open(file, 'rb' ) )
+    for i in range(len(registers)):
+        registers[i]['id'] = i
+    return(registers)
     
-
+def create_dataframe(registers):
+    info_keys = ['id','candidate_for','first_name','last_name','gender',
+    'investigations','list_number']
+    candidates_df = pd.DataFrame(registers, columns=info_keys)
+    parties = [] 
+    trajectories  = [] 
+    topics =[]
+    for rec in registers:
+    # The value name in party is replaced by  party_name that is more general
+        party = {'id':rec['id'],'party_name':rec['party']['name']}
+        trajectory = {'id':rec['id'],
+        'highlighted_projects':rec['trajectory']['highlighted_projects'],
+        'years_in_congress': rec['trajectory']['years_in_congress'],
+        'main_topics': rec['trajectory']['main_topics'], 
+        'politic_control_summonses': rec['trajectory']['politic_control_summonses']}
+        topic = {'id':rec['id']}
+        for topic_position in rec['topics_positions']:
+            topic[topic_position['name']] = topic_position['posicion']
+        parties.append(party)
+        trajectories.append(trajectory)
+        topics.append(topic)
+    parties_df = pd.DataFrame(parties)
+    trajectories_df = pd.DataFrame(trajectories)
+    topics_df = pd.DataFrame(topics)
+    candidates_df = pd.merge(candidates_df,parties_df)
+    candidates_df = pd.merge(candidates_df,trajectories_df)
+    candidates_df  = pd.merge(candidates_df,topics_df)
+    return(candidates_df)
+    
+    
+    
+    
